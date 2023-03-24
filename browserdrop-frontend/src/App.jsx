9048e9b1b217547478470cedc23264d8eebf9a15
happycoder0011 <sheetalsinghnew@gmail.com>
2023-03-24 19:26:59 +0530

    feat: :art: Frontend Boilerplate code

---
+++ browserdrop-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.scss";
+
+const App = () => {
+
+
+  return(
+    <div>hOME</div>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
